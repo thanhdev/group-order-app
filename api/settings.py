b6884d027d95bddc3bab1f89e79af771e5aa20df
tthanh <ytominhthanh@gmail.com>
2024-05-28 10:12:43 +0700

    Disable DEBUG

--- api/settings.py
+++ api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app"]
 
