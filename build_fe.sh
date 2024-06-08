ecb66e8325a3ef78fd27d5eeabbe8d63c3a51350
tthanh <ytominhthanh@gmail.com>
2024-06-08 10:25:43 +0700

    Update scripts

--- build_fe.sh
+++ build_fe.sh
@@ -1,7 +1,7 @@
 cd group-order
 git pull origin gh-pages
 cd ..
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput -c
 pre-commit run -a
 git add static/
 git commit -m "build frontend"
